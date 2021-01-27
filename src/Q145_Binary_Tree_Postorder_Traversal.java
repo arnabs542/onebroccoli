cfe8621f3fbaafa5435ed51e2310163c1263486b
onebroccoli <three.broccoli@gmail.com>
2021-01-27 15:36:41 -0800

    binary tree

---
+++ src/Q145_Binary_Tree_Postorder_Traversal.java
@@ -0,0 +1,80 @@
+/*
+Given the root of a binary tree,
+return the postorder traversal of its nodes' values.
+
+
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [2,1]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [2,1]
+
+
+Constraints:
+
+The number of the nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+
+Follow up:
+
+Recursive solution is trivial, could you do it iteratively?
+
+
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Q145_Binary_Tree_Postorder_Traversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+
+    }
+    public void helper(TreeNode root, List<Integer> res){
+        if (root == null){
+            return;
+        }
+
+        helper(root.left, res);
+        helper(root.right, res);
+        res.add(root.val);
+    }
+}
