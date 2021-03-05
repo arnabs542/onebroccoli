36a5c51b6b832a7f16f933c88010051a94499b7b
onebroccoli <three.broccoli@gmail.com>
2021-03-04 23:52:00 -0800

    permutation

--- src/Q46_Permutations.java
+++ src/Q46_Permutations.java
@@ -23,5 +23,46 @@ Constraints:
 -10 <= nums[i] <= 10
 All the integers of nums are unique.
  */
+
+import java.util.List;
+import java.util.ArrayList;
 public class Q46_Permutations {
+    public List<String> permutations(String set){
+        List<String> result = new ArrayList<String>();
+        //sanity check
+        if (set == null){
+            return result;
+        }
+        char[] array = set.toCharArray();
+        swap(array, 0, result);
+        return result;
+    }
+
+    //choose the character to be at the position of "Index"
+    //all the already chosen positions are (0, index - 1)
+    //all the candidate characters can be at position "index"
+    //are in the subarray of (index, array.length-1)
+    private void helper(char[] array, int index,  List<String> result){
+        //terminate condition:
+        //only when we have already chosen the characters for all the positions,
+        //we can have a complete permutation
+        if (index == array.length){
+            result.add(new String(array));
+            return;
+        }
+        //all the possible characters could be placed at index are
+        //the characters in the subarray (index, array.length-1)
+        for (int i = index; i < array.length; i++){
+            swap(array, index, i);
+            helper(array, index + 1, result);
+            //remember to swap back when back track to previou level
+            swap(array, index, 1);
+        }
+    }
+
+    private void swap(char[] array, int left, int right){
+        char tmp = array[left];
+
+    }
+
 }
