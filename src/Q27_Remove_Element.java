75b1c2328910b47a1bfa8e56cb496f44e1866986
onebroccoli <three.broccoli@gmail.com>
2021-03-11 23:46:43 -0800

    remove elements

--- src/Q27_Remove_Element.java
+++ src/Q27_Remove_Element.java
@@ -27,13 +27,19 @@ Example 1:
 
 Input: nums = [3,2,2,3], val = 3
 Output: 2, nums = [2,2]
-Explanation: Your function should return length = 2, with the first two elements of nums being 2.
-It doesn't matter what you leave beyond the returned length. For example if you return 2 with nums = [2,2,3,3] or nums = [2,2,0,0], your answer will be accepted.
+Explanation: Your function should return length = 2,
+with the first two elements of nums being 2.
+It doesn't matter what you leave beyond the returned length.
+For example if you return 2 with nums = [2,2,3,3] or nums = [2,2,0,0],
+your answer will be accepted.
 Example 2:
 
 Input: nums = [0,1,2,2,3,0,4,2], val = 2
 Output: 5, nums = [0,1,4,0,3]
-Explanation: Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4. Note that the order of those five elements can be arbitrary. It doesn't matter what values are set beyond the returned length.
+Explanation: Your function should return length = 5,
+with the first five elements of nums containing 0, 1, 3, 0, and 4.
+Note that the order of those five elements can be arbitrary.
+It doesn't matter what values are set beyond the returned length.
 
 
 Constraints:
@@ -43,4 +49,7 @@ Constraints:
 0 <= val <= 100
  */
 public class Q27_Remove_Element {
+    public int removeElement(int[] nums, int val){
+
+    }
 }
