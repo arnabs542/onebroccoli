4485ac68c3027d87b9b49c208fc01b826c4e2462
onebroccoli <three.broccoli@gmail.com>
2021-03-23 13:54:26 -0700

    string replace

--- src/StringReplace.java
+++ src/StringReplace.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /*
 Given an original string input, and two strings S and T,
 from left to right replace all occurrences of S in input with T.
@@ -48,7 +50,41 @@ public class StringReplace {
     }
 
     //handle s1<s2
-
+    //notice: we will need a longer array in the case, and if the requirement is
+    //in place, usually you can assume you are given a long enough
+    //char array already, the original input string resides
+    //on part of the char array starting from index 0.
+    //in this solution, we actually allocate a larger array on demand, and
+    //the purpose of the solution is to demonstrate how to do it in place
+    public String replaceLonger(char[] input, String s1, String s2){
+        //get all the matches end positions in the input char array of string s1
+        ArrayList<Integer> matches = getAllMatches(input, s1);
+        //calculate the new length needed
+        char[] result = new char[input.length + matches.size() * (s2.length() - s1.length())];
+        //fast and slow pointers both from right to left direction
+        //slow: position when traversing from the new length
+        //fast: position when traversing the old length
+        //allocate a larger array on demand and the purpose of the solution is to demonstrate how
+        // to do it in place
+        int lastIndex = matches.size() - 1;
+        int fast = input.length - 1;
+        int slow = result.length - 1;
+        while (fast >= 0){
+            //only if we still have some match and slow is in the position of
+            //rightmost matching end position, we should copy it
+            if (lastIndex >= 0 && fast == matches.get(lastIndex)){
+                copySubstring(result, slow - s2.length() + 1, s2);
+                slow -= s2.length();
+                fast -= s1.length();
+                lastIndex--;
+            } else {
+                result[slow] = input[fast];
+                slow--;
+                fast--;
+            }
+        }
+        return new String(result);
+    }
     //check if the substring from fromIndex is the same as s1
     private boolean equalSubstring(char[] input, int fromIndex, String s){
         for (int i = 0; i < s.length(); i++){
@@ -64,5 +100,30 @@ public class StringReplace {
         for (int i = 0; i < s2.length(); i++){
             result[fromIndex + i] = s2.charAt(i);
         }
+    }
+
+    //get all the matches of s1 end positions in input
+    private ArrayList<Integer> getAllMatches(char[] input, String s){
+        ArrayList<Integer> matches = new ArrayList<Integer>();
+        int i = 0;
+        while (i <= input.length - s.length()){
+           if (equalSubstring(input, i , s)){
+               //record the match substring's end index instead of start index for later convenience.
+               matches.add(i + s.length() - 1);
+               i += s.length();
+           } else {
+               i++;
+           }
+        }
+        return matches;
+    }
+
+    public static void main(String[] args){
+        StringReplace s = new StringReplace();
+        String a1 = "student";
+        String s1 = "den";
+        String s2 = "ooo";
+        String result = s.replaceI(a1, s1, s2);
+        System.out.println(result);
     }
 }
