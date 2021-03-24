736629d762953bdebc85f92d6df37a2630205a9f
onebroccoli <three.broccoli@gmail.com>
2021-03-24 16:53:19 -0700

    all permutations with paratethesis

--- src/AllPermutations.java
+++ src/AllPermutations.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AllPermutations {
     public List<String> validParentheses(int n) {
         List<String> result = new ArrayList<String>();
