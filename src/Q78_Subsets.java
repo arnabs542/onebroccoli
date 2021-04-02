ab7a4f8f87804fb90aa4ef9f0e07c275b6b67c50
onebroccoli <three.broccoli@gmail.com>
2021-04-02 13:32:59 -0700

    subsets string and int[]

--- src/Q78_Subsets.java
+++ src/Q78_Subsets.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -15,7 +16,7 @@ Set = "", all the subsets are [""]
 Set = null, all the subsets are []
  */
 public class Q78_Subsets {
-    public List<String> subsets(String set){
+    public List<String> subsetsString(String set){
         List<String> result = new ArrayList<>();
         if (set == null){
             return result;
@@ -41,11 +42,50 @@ public class Q78_Subsets {
         sb.deleteCharAt(sb.length() - 1); //remember to remove the added character when backtracking to previous level
     }
 
+    public List<List<Integer>> subsetsInteger(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null){
+            return result;
+        }
+//        List<Integer> cur = new ArrayList<>();
+        helperInteger(nums, new ArrayList<>(), 0, result);
+        return result;
+    }
+
+    public void helperInteger(int[] nums, List<Integer> cur, int index, List<List<Integer>> result){
+        if (index == nums.length){
+            List<Integer> copyList = new ArrayList<>();
+            // create new instance of each element of list and add to the result
+            //不能直接add因为后面backtracking的时候remove后，result里只存的reference也会被清掉
+            for (Integer n : cur)
+            {
+
+                copyList.add(n);
+            }
+            result.add(copyList);
+            return;
+        }
+        //not pick up the int at index
+        helperInteger(nums, cur, index + 1, result);
+
+        //pick up the int at index
+        cur.add(nums[index]);
+        helperInteger(nums, cur, index + 1, result);
+        cur.remove(cur.size() - 1);
+    }
+
     public static void main(String[] args){
         Q78_Subsets s = new Q78_Subsets();
         String a = "abc";
-        List<String> result = s.subsets(a);
-        System.out.println(result);
+        System.out.println("input is: " + a);
+        List<String> result1 = s.subsetsString(a);
+        System.out.println(result1);
+
+        int[] b = {1,2,3};
+        System.out.println("input is: " + Arrays.toString(b));
+        List<List<Integer>> result2 = s.subsetsInteger(b);
+        System.out.println(result2);
+
 
     }
 }
