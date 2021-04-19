68fd86b052519e697b3cefcbd74ae3d680644725
onebroccoli <three.broccoli@gmail.com>
2021-04-18 17:38:49 -0700

    reverse linkedlist in pairs

--- src/ReverseLinkedListInPairs.java
+++ src/ReverseLinkedListInPairs.java
@@ -8,5 +8,19 @@ L = 1 -> null, after reverse is 1 -> null
 L = 1 -> 2 -> null, after reverse is 2 -> 1 -> null
 L = 1 -> 2 -> 3 -> null, after reverse is 2 -> 1 -> 3 -> null
  */
+//1->2->3->4->5
+
 public class ReverseLinkedListInPairs {
+    //solution1: recursion
+    public ListNode reverseInPairs(ListNode head){
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode newHead = head.next; //new head
+        head.next = reverseInPairs(head.next.next);
+        newHead.next = head;
+        return newHead;
+    }
+
+    //solution1: iterative
 }
