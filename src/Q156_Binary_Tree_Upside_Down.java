e2516f8ce7f49fefb341b09b8dad529ea349df43
onebroccoli <three.broccoli@gmail.com>
2021-04-18 22:14:45 -0700

    reverse binary tree upside down

--- src/Q156_Binary_Tree_Upside_Down.java
+++ src/Q156_Binary_Tree_Upside_Down.java
@@ -39,4 +39,16 @@ solution
 
  */
 public class Q156_Binary_Tree_Upside_Down {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        //recursion
+        if (root == null || root.left == null){
+            return root;
+        }
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
 }
