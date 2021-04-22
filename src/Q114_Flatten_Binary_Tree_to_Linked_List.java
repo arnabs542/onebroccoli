2ae8dcc7869510c588f9960fdf8c2d957618c99c
onebroccoli <three.broccoli@gmail.com>
2021-04-21 23:33:37 -0700

    flatten binary tree to linked list

---
+++ src/Q114_Flatten_Binary_Tree_to_Linked_List.java
@@ -0,0 +1,45 @@
+/*
+Given the root of a binary tree, flatten the tree into a "linked list":
+
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+
+Example 1:
+
+
+Input: root = [1,2,5,3,4,null,6]
+Output: [1,null,2,null,3,null,4,null,5,null,6]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [0]
+Output: [0]
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+
+ */
+public class Q114_Flatten_Binary_Tree_to_Linked_List {
+    public void flatten(TreeNode root) {
+        TreeNode[] prev = new TreeNode[1];
+        helper(root, prev);
+    }
+
+    private void helper(TreeNode root, TreeNode[] prev){
+        if (root == null){
+            return;
+        }
+        helper(root.right, prev);
+        helper(root.left, prev);
+        root.right = prev[0];
+        root.left = null;
+        prev[0] = root;
+    }
+}
