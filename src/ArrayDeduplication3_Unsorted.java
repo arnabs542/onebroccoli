c05ff398f601bac137e0e0be68f61ab3be09fbef
onebroccoli <three.broccoli@gmail.com>
2021-04-22 11:42:21 -0700

    array deduplication3

--- src/ArrayDeduplication3_Unsorted.java
+++ src/ArrayDeduplication3_Unsorted.java
@@ -11,25 +11,18 @@ Assumptions
 The given array is not null
 Examples
 
-{1, 2, 2, 3, 3, 3} → {1}
-祖玛
+{1, 2, 2, 3, 3, 3} → {1 2 3}
  */
 
 import java.util.Arrays;
 
-/*
-solution:
-1. 用stack会有额外空间
-2. 用two pointer更好
-
- */
 public class ArrayDeduplication3_Unsorted {
     public int[] dedup(int[] array){
         //assumption: array is not null
-        if (array != null || array.length <= 1){
+        if (array == null || array.length <= 1){
             return array;
         }
-        //1 2 2 2 1 3
+
         //use flag to see if there is any duplicates of array[slow]
         boolean flag = false;
         int slow = 0;
@@ -37,24 +30,29 @@ public class ArrayDeduplication3_Unsorted {
             //case 1: if there is duplicate, set flag and do nothing
             if (array[fast] == array[slow]){
                 flag = true;
+                //array[fast] != array[slow]
+                //case2.1 flag = true: fast 走到了终于不重复的值
             } else if (flag == true){
                 array[slow] = array[fast];
                 //reset flag to false since we start processing another element
                 flag = false;
             } else {
+                //case2.2 flag = false
                 //if array[fast] != array[slow] && flag is not set
                 // means there is no duplicate of array[slow] and
                 // should be included in the valid subarray
-                slow++;
-                array[slow] = array[fast];
+                array[++slow] = array[fast];
             }
         }
         return Arrays.copyOfRange(array, 0, flag ? slow : slow + 1);
     }
 
+    //{1 2 2 2 1 3 3} ==> {1 1 3}
+
+
     public static void main(String[] args) {
         ArrayDeduplication3_Unsorted s = new ArrayDeduplication3_Unsorted();
-        int[] a = {1, 2, 2, 2, 1, 3};
+        int[] a = {1, 2, 2, 2, 1, 3, 3};
         int[] result = s.dedup(a);
         for (int i : result) {
             System.out.println(i);
