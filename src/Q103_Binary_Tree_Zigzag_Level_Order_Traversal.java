f2b95e55817ec48f6adc1ef8f3670cd437134b82
onebroccoli <three.broccoli@gmail.com>
2021-04-26 23:55:18 -0700

    binary tree zigzag level order traversal

---
+++ src/Q103_Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+/*
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+
+
+Example 1:
+   3
+  / \
+9   20
+    / \
+   15  7
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+
+ */
+public class Q103_Binary_Tree_Zigzag_Level_Order_Traversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root){
+
+    }
+}
