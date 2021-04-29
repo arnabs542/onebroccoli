0ecf2935ebce6e3f043fbb50acc6557396430886
onebroccoli <three.broccoli@gmail.com>
2021-04-29 15:42:31 -0700

    3sum

--- src/Q15_3sum.java
+++ src/Q15_3sum.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
 Given an integer array nums, return all the triplets
 [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
@@ -16,5 +20,37 @@ Input: nums = []
 Output: []
  */
 public class Q15_3sum {
-    public
+    public List<List<Integer>> threeSum(int[] array, int target){
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Arrays.sort(array);
+        for (int i = 0; i < array.length - 2; i++){
+            if (i > 0 && array[i] == array[i - 1]){
+                continue;
+            }
+            int left = i + 1;
+            int right = array.length - 1;
+            while (left < right){
+                int tmp = array[left] + array[right];
+                if (tmp + array[i] == target){
+                    result.add(Arrays.asList(array[i], array[left], array[right]));
+                    left++;
+                }
+                if (left < right && array[left] == array[left - 1]){
+                    left++;
+                } else if (tmp + array[i] < target){
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        Q15_3sum s = new Q15_3sum();
+        int[] array = {1,1,2,3,4,5,6,3,5};
+        int target = 11;
+        List<List<Integer>> result = s.threeSum(array,target );
+        System.out.println(result);
+    }
 }
