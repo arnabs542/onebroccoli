41111cea7ec481372c37f12e8aab377f53bcb828
onebroccoli <three.broccoli@gmail.com>
2021-05-04 23:56:50 -0700

    BST distance

---
+++ src/distanceInBST.java
@@ -0,0 +1,48 @@
+/*
+ Distance between two given nodes in a given binary search tree.
+  Assuming:
+
+there is no parent pointer.
+
+both nodes belong to the tree.
+
+           15
+
+        /          \
+
+      5           20
+
+     /    \
+
+   3      10
+
+ /   \      /
+
+1   4    8
+
+          /   \
+
+         7     9
+
+
+
+distance(3, 8) = 3
+
+distance(5, 7) = 3
+
+distance(1, 9) = 5
+
+
+
+class TreeNode {
+
+int val;
+
+TreeNode left;
+
+TreeNode right;
+
+}
+ */
+public class distanceInBST {
+}
