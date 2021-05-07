8559a66d84c10ca583afb8dd8e9eda1ff74e6b58
onebroccoli <three.broccoli@gmail.com>
2021-05-07 01:52:24 -0700

    common elements in 3 sorted array

--- src/CommonElementsInThreeSortedArray.java
+++ src/CommonElementsInThreeSortedArray.java
@@ -1,5 +1,14 @@
 /*
 171. Common Elements In Three Sorted Array LAI
+Find all common elements in 3 sorted arrays.
+
+Assumptions
+
+The 3 given sorted arrays are not null
+There could be duplicate elements in each of the arrays
+Examples
+
+A = {1, 2, 2, 3}, B = {2, 2, 3, 5}, C = {2, 2, 4}, the common elements are [2, 2]
  */
 public class CommonElementsInThreeSortedArray {
 
