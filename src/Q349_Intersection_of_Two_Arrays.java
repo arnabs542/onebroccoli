916d4bffc7c7d15c8a2c6c0cdb9d2327ff9761a8
onebroccoli <three.broccoli@gmail.com>
2021-05-07 10:50:22 -0700

    intersection of two array

--- src/Q349_Intersection_of_Two_Arrays.java
+++ src/Q349_Intersection_of_Two_Arrays.java
@@ -3,18 +3,28 @@ import java.util.HashSet;
 
 /*
 349. Intersection of Two Arrays Leetcode
-651. Common Numbers Of Two Arrays II(Array version) LAI
-Find all numbers that appear in both of two unsorted arrays.
-
-Assumptions
-
-Both of the two arrays are not null.
-In any of the two arrays, there could be duplicate numbers.
-Examples
-
-A = {1, 2, 3, 2}, B = {3, 4, 2, 2, 2}, return [2, 2, 3] (there are both two 2s in A and B)
+Given two integer arrays nums1 and nums2,
+return an array of their intersection.
+Each element in the result must be unique
+and you may return the result in any order.
 
 
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
 
  */
 
