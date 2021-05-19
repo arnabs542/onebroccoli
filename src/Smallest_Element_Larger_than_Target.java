244ae263a2c96bbda0fa10b29a117499deb183ce
onebroccoli <three.broccoli@gmail.com>
2021-05-18 22:09:56 -0700

    SMALLEST

--- src/Smallest_Element_Larger_than_Target.java
+++ src/Smallest_Element_Larger_than_Target.java
@@ -1,5 +1,30 @@
 /*
 636. Smallest Element Larger than Target LAI
+Given a target integer T and an integer array A sorted in ascending order,
+find the index of the smallest element in A that is larger than T or
+return -1 if there is no such index.
+
+
+
+Assumptions
+
+There can be duplicate elements in the array.
+
+
+
+Examples
+
+A = {1, 2, 3}, T = 1, return 1
+
+A = {1, 2, 3}, T = 3, return -1
+
+A = {1, 2, 2, 2, 3}, T = 1, return 1
+
+
+
+Corner Cases
+
+What if A is null or A of zero length? We should return -1 in this case.
  */
 public class Smallest_Element_Larger_than_Target {
 }
